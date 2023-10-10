a31989e277b61bb159a745e11657699b2e8f05c3
Danny123-coder-spec <99043002+Danny123-coder-spec@users.noreply.github.com>
2023-10-10 20:20:56 +0000

    Add files via upload

---
+++ src/Data/Sellers.js
@@ -0,0 +1,33 @@
+import friedrice from '../assets/pizza/friedrice5.jpg'
+import orange from '../assets/pizza/yam2.jpg'
+import plantain from '../assets/pizza/jollof(bucket).jpg'
+import noodle from '../assets/pizza/NOODLES.jpg'
+
+const SellersData = [
+    {
+        id:1,
+        img:friedrice,
+        title:'Drums 8PCS',
+        price:'11.90',
+    },
+    {
+        id:1,
+        img:orange,
+        title:'Yam Chips + 3 Wings',
+        price:'11.90',
+    },
+    {
+        id:1,
+        img:plantain,
+        title:'Jollof Mini Bucket',
+        price:'11.90',
+    },
+    {
+        id:1,
+        img:noodle,
+        title:'Kersame Pizza',
+        price:'11.90',
+    },
+]
+
+export default SellersData;
